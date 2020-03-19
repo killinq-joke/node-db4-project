dfff7b1976cca4961b2fb832a2ab8e885ad90b0e
Zak <zakadmaster@gmail.com>
2020-03-19 16:48:20 +0100

    Reformatting

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const express = require("express")
-const helmet = require("helmet")
-const cors = require("cors")
-const recipesRouter = require("./routers/recipesRouter")
-const ingredientsRouter = require("./routers/ingredientsRouter")
+const path = require("path");
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const recipesRouter = require("./routers/recipesRouter");
+const ingredientsRouter = require("./routers/ingredientsRouter");
 
-const server = express()
+const server = express();
 
-server.use(express.json())
-server.use(cors())
-server.use(helmet())
-server.use("/api/recipes", recipesRouter)
-server.use("/api/ingredients", ingredientsRouter)
+server.use(express.static(path.join(__dirname, "client/build")));
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use("/api/recipes", recipesRouter);
+server.use("/api/ingredients", ingredientsRouter);
 
-module.exports = server
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
+module.exports = server;
