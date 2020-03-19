589bb31180de72d66499cc98ba4920c070658a58
Zak <zakadmaster@gmail.com>
2020-03-19 15:21:30 +0100

    Addition of getShoppingList

--- helpers/recipesHelpers.js
+++ helpers/recipesHelpers.js
@@ -1,19 +1,32 @@
-const db = require("../data/db-config")
+const db = require("../data/db-config");
 
 module.exports = {
-    getRecipes,
-    getShoppingList,
-    getInstructions
-}
+  getRecipes,
+  getShoppingList,
+  getInstructions
+};
 
 function getRecipes() {
-    return db("recipes")
+  return db("recipes");
 }
 
-function getShoppingList(recipe_id) {
-    return db("recipes")
+function getShoppingList(id) {
+  return db
+    .select("ingredient_name", "ingredient_qty")
+    .from("recipes")
+    .join("steps", "steps.recipe_id", "=", "recipes.id")
+    .join("ingredients", "ingredients.id", "=", "steps.ingredient_id")
+    .where("recipes.id", id);
 }
+
+// select ingredient_qty, ingredient_name
+// from recipes as r
+// join steps as s
+//     on s.recipe_id = r.id
+// join ingredients as i
+//     on i.id = s.ingredient_id
+// where r.id = 2
 
 function getInstructions(recipe_id) {
-    return db("recipes")
+  return db("recipes");
 }
