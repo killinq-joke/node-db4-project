31c3bd4d0e4e7a81e80d80000b5d3a6364cd906a
Zak <zakadmaster@gmail.com>
2020-03-19 16:12:50 +0100

    Addition of CRA

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const express = require("express")
-const helmet = require("helmet")
-const cors = require("cors")
-const recipesRouter = require("./routers/recipesRouter")
-const ingredientsRouter = require("./routers/ingredientsRouter")
-
-const app = express()
+require("dotenv").config()
+const server = require("./server")
 
 const PORT = process.env.PORT || 5000
 
-app.use(express.json())
-app.use(cors())
-app.use(helmet())
-app.use("/api/recipes", recipesRouter)
-app.use("/api/ingredients", ingredientsRouter)
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 })
